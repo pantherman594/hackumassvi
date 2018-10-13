fec0ba9840cb75f175c6f766958c2f428356cbfd
PantherMan594 <me@pantherman594.com>
2018-10-13 00:59:06 -0400

    Facebook login

---
+++ app/src/main/java/com/pantherman594/gitzucccd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pantherman594.gitzucccd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Put the camera here
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
